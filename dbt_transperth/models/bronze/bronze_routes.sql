9ed5272f5046689994f886a6e6ff02e23b37da5a
jasonzelin <jr9808@gmail.com>
2026-07-17 16:02:55 +0800

    fix: modify bronze and gold schema and source files to exclude some not null tests

--- dbt_transperth/models/bronze/bronze_routes.sql
+++ dbt_transperth/models/bronze/bronze_routes.sql
@@ -2,22 +2,7 @@ with source as (
     select * from {{ source('google_transit', 'routes') }}
 )
 
-,raw as (
-    select
-        route_id
-        ,route_short_name
-        ,route_long_name
-        ,cast(route_type as integer) as route_type
-        ,case cast(route_type as integer)
-            when 2 then 'Rail'
-            when 3 then 'Bus'
-            when 4 then 'Ferry'
-        end as route_type_label
-    from
-        source
-)
-
 select
     *
 from
-    raw
+    source
